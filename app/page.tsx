9bf310eb2a02e4b33dadfd89f68ea885ccd749c3
Martin Ouimet <mouimet@infinisoft.world>
2023-11-01 03:43:58 -0400

    ssr disabled onn main page

--- app/page.tsx
+++ app/page.tsx
@@ -1,17 +1,23 @@
-"use client"
 //@ts-nocheck
-import React from 'react'
-import AgentsPage from "./retrieval_agents/page";
-import brainstack from '@/app/hooks/brainstack'
-const { BrainStackProvider } = brainstack
+"use client"
+import React from 'react';
+import dynamic from 'next/dynamic';
 
+// Dynamically import BrainStackProvider and AgentsPage
+const BrainStackProvider = dynamic(
+  () => import('@/app/hooks/brainstack').then(mod => mod.default.BrainStackProvider),
+  { ssr: false }  // Load BrainStackProvider only on the client side
+);
+
+const AgentsPage = dynamic(
+  () => import('./retrieval_agents/page'),
+  { ssr: false }  // Load AgentsPage only on the client side
+);
 
 export default function Home() {
   return (
-    <>
-      <BrainStackProvider>
-        <AgentsPage />
-      </BrainStackProvider>
-    </>
+    <BrainStackProvider>
+      <AgentsPage />
+    </BrainStackProvider>
   );
 }
